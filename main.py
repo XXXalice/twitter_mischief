ce524a1d86642d49122a39a9c33b2c49d68a5cea
梅宮 <euthanasia045@gmail.com>
2019-09-24 19:13:52 +0900

    add main

---
+++ main.py
@@ -0,0 +1,4 @@
+from .scrape import Scrape
+from .db import Database
+
+scraper = Scrape()
--- scrape.py
+++ scrape.py
@@ -1,3 +1,8 @@
 from bs4 import BeautifulSoup
 from urllib import request
+from .etc import driver, logger
 
+class Scrape:
+    def __init__(self):
+        yml_path = './config.yml'
+        self.param = driver.read_yaml(yml_path)
