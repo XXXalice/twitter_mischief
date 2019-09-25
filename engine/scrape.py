8eaf39fb97c839e832db41b539a83ba64295b4a4
梅宮 <euthanasia045@gmail.com>
2019-09-25 16:33:17 +0900

    query構築関数導入

--- engine/scrape.py
+++ engine/scrape.py
@@ -6,3 +6,25 @@ class Scrape:
     def __init__(self):
         yml_path = './config.yml'
         self.param = driver.read_yaml(yml_path)
+
+
+    def crawl(self, **attrs):
+        uri = self.__construct_query(attrs)
+        req = request.Request(url=uri)
+        with request.urlopen(req) as resp:
+            b = resp.read()
+
+    def __construct_query(self, attrs):
+        base = self.param['server']['api']
+        base += '?'
+        items_checker = {
+            'lang':"lang={}",
+            'user':"(from:%3A{})",
+            'word':"{}"
+        }
+        for k, v in items_checker.items():
+            if k in attrs.keys():
+                base += v.format(attrs[k])
+            base += '&'
+
+        return base
